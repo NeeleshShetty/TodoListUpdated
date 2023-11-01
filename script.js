d7f29fc6b53638ea201c5a336d3067007e8a5bc8
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2023-11-01 22:29:29 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('Please enter a task')
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let img = document.createElement("img");
+        img.src = "delete.png"
+        img.className="delete";
+        li.append(img);
+        
+    }
+    
+    inputBox.value = "";
+    saveData();
+   
+}
+
+
+listContainer.addEventListener('click',(e)=>{
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            saveData();
+        }
+        else if(e.target.tagName === "IMG"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData();
